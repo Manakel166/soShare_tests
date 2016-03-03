a703c5166c17daaebd6c7f41bf47ab9a13938e47
GROCHULSKI <NG1A95C@eu.airbus.corp>
2016-03-03 02:00:09 +0100

    add_smoke_tests_launcher

--- smoke_tests.bat
+++ smoke_tests.bat
@@ -1,2 +1,2 @@
 mkdir %BUILD_NUMBER% 
-call "E:\RF_STUDIO\00_CONSOLE\pybot_portable.bat" %WORKSPACE%\soShare_BDD.robot --include SMOKE --exclude NOT_READY  --output %BUILD_NUMBER%\
+call "E:\RF_STUDIO\00_CONSOLE\pybot_portable.bat" %WORKSPACE%\soShare_BDD.robot --include SMOKE --exclude NOT_READY  --output %BUILD_NUMBER%
